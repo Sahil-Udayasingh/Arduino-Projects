14d90c51e71a533f73d76be1891c24fac1347201
Sahil Udayasingh <48101923+Sahil1216@users.noreply.github.com>
2019-07-27 23:45:19 +0530

    Add files via upload

---
+++ Projects/Servo_using_button.ino
@@ -0,0 +1,21 @@
+
+#include<Servo.h>
+Servo x;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(A0,INPUT);
+  x.attach(11);
+  Serial.begin(9600);
+}
+
+void loop() {
+  
+  int value=analogRead(A0);
+  Serial.println(value);
+  //delay(1000);
+  if(value==0)
+  x.write(90);
+  else
+  x.write(1);
+}
