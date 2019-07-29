6bea5fd30215f72d73afae96b3171b56c0fef05c
Sahil Udayasingh <48101923+Sahil1216@users.noreply.github.com>
2019-07-30 00:41:56 +0530

    Update Auto_Lighting.ino

--- Projects/Auto Lighting/Auto_Lighting.ino
+++ Projects/Auto Lighting/Auto_Lighting.ino
@@ -8,15 +8,15 @@ int c=0;
 void setup() {
   pinMode(3,INPUT);
   pinMode(4,INPUT);
-  pinMode(11,OUTPUT);
-  pinMode(13,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  if(analogRead(3)<500)
+  if(digitalRead(3)<500)
   c++;
-  if(analogRead(4)<500)
+  if(digitalRead(4)<500)
   c--;
   Serial.println(c);
   delay(500);
@@ -35,10 +35,10 @@ void loop() {
   delay(1000);
 
   if(c>0&&t>27)
-  digitalWrite(11,HIGH);
+  digitalWrite(5,HIGH);
   if(c==0)
-  digitalWrite(11,LOW);
+  digitalWrite(5,LOW);
   if(c>0)
-  digitalWrite(13,HIGH);
+  digitalWrite(6,HIGH);
    
 }
