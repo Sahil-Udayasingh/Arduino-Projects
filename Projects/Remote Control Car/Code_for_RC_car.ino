10e177951fd051b8092d6244007a34bf8d36de19
Sahil Udayasingh <48101923+Sahil1216@users.noreply.github.com>
2019-10-01 10:05:21 +0530

    Add to Arduino

--- Projects/Remote Control Car/Code_for_RC_car.ino
+++ Projects/Remote Control Car/Code_for_RC_car.ino
@@ -6,8 +6,8 @@ int motorPin5 = 9; // pin 2 on L293D IC
 int motorPin6 = 8; // pin 7 on L293D IC
 int motorPin7 = 7; // pin 2 on L293D IC
 int motorPin8 = 6; // pin 7 on L293D IC
-int state;
-//int flag=0; 
+char state;
+
 
 void setup() {
     // sets the pins as outputs:
@@ -26,39 +26,41 @@ void loop() {
     //if some date is sent, reads it and saves in state
     if(Serial.available() > 0){
 state = Serial.read();
-//flag=0;
 }
     // if the state is '0' the DC motor will turn off
-    if (state == '0') {
+    if (state == '0') 
+    {
         digitalWrite(motorPin1, LOW); // set pin 2 on L293D low
         digitalWrite(motorPin2, LOW); // set pin 7 on L293D low
+        
         digitalWrite(motorPin3, LOW); // set pin 2 on L293D low
         digitalWrite(motorPin4, LOW); // set pin 7 on L293D low
+        
         digitalWrite(motorPin5, LOW); // set pin 2 on L293D low
         digitalWrite(motorPin6, LOW); // set pin 7 on L293D low
+        
         digitalWrite(motorPin7, LOW); // set pin 2 on L293D low
         digitalWrite(motorPin8, LOW); // set pin 7 on L293D low
-//        if(flag == 0){
-//        Serial.println("Motor: off");
-//        flag=1;
-}
-}
-    // if the state is '1' the motor will turn right
-    else if (state == 'F') {
+   }
+    
+   else if (state == 'F') 
+   {
         digitalWrite(motorPin1, HIGH); // set pin 2 on L293D low
         digitalWrite(motorPin2, LOW ); // set pin 7 on L293D high
+        
         digitalWrite(motorPin3, LOW); // set pin 2 on L293D low
         digitalWrite(motorPin4, HIGH); // set pin 7 on L293D high
+        
         digitalWrite(motorPin5, HIGH); // set pin 2 on L293D low
         digitalWrite(motorPin6, LOW); // set pin 7 on L293D high
+        
         digitalWrite(motorPin7, HIGH); // set pin 2 on L293D low
         digitalWrite(motorPin8, LOW); // set pin 7 on L293D high
-//        if(flag == 0){
-//Serial.println("Motor: right");
-//flag=1;
-}
-}
-    else if (state == 'R'){
+
+    }
+    
+    else if (state == 'R')
+    {
         
         digitalWrite(motorPin1, HIGH); // set pin 2 on L293D low
         digitalWrite(motorPin2, LOW); // set pin 7 on L293D high
@@ -71,41 +73,36 @@ state = Serial.read();
         
         digitalWrite(motorPin7, LOW); // set pin 2 on L293D low
         digitalWrite(motorPin8, HIGH); // set pin 7 on L293D high
-//        if(flag == 0){
-//            Serial.println("Motor: right");
-//            flag=1;
-//        }
     }
-    else if (state == 'L'){
+    
+    else if (state == 'L')
+    {
         
         digitalWrite(motorPin1, LOW); // set pin 2 on L293D low
         digitalWrite(motorPin2, HIGH); // set pin 7 on L293D high
         
         digitalWrite(motorPin3, LOW); // set pin 2 on L293D low
         digitalWrite(motorPin4, HIGH); // set pin 7 on L293D high
+
         digitalWrite(motorPin5, LOW); // set pin 2 on L293D low
         digitalWrite(motorPin6, HIGH); // set pin 7 on L293D high
         
         digitalWrite(motorPin7, HIGH); // set pin 2 on L293D low
         digitalWrite(motorPin8, LOW); // set pin 7 on L293D high
-//        if(flag == 0){
-//            Serial.println("Motor: right");
-//            flag=1;
-//        }
     }
-    // if the state is '2' the motor will turn left
-    else if (state == 'B') {
+
+    else if (state == 'B')
+    {
         digitalWrite(motorPin1, LOW); // set pin 2 on L293D high
         digitalWrite(motorPin2, HIGH); // set pin 7 on L293D low
+        
         digitalWrite(motorPin3, HIGH); // set pin 2 on L293D high
         digitalWrite(motorPin4, LOW); // set pin 7 on L293D low
+        
         digitalWrite(motorPin5, LOW); // set pin 2 on L293D high
         digitalWrite(motorPin6, HIGH); // set pin 7 on L293D low
+        
         digitalWrite(motorPin7, LOW); // set pin 2 on L293D high
         digitalWrite(motorPin8, HIGH); // set pin 7 on L293D low
-//        if(flag == 0){
-//Serial.println("Motor: left");
-//flag=1;
-//}
-}
+    }
 }
