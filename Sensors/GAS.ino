a6efd3481f8d2775beaba993c426d7f3d75ab48a
Sahil Udayasingh <48101923+Sahil1216@users.noreply.github.com>
2019-12-20 00:23:19 +0530

    Add to Arduino

---
+++ Sensors/GAS.ino
@@ -0,0 +1,15 @@
+int smokeA0 = A5;
+int sensorThreshold = 400;
+
+void setup() {
+  pinMode(smokeA0, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int analogSensor = analogRead(smokeA0);
+
+  Serial.print("Pin A0: ");
+  Serial.println(analogSensor);
+  delay(100);
+}
